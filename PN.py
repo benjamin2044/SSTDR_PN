e32872d0f2f5753cd7393ecfa0e7534658b37424
Barun <barunbasnet1@gmail.com>
2018-07-26 22:41:46 +0900

    Update PN.py

--- PN.py
+++ PN.py
@@ -1,59 +1,68 @@
+
 ##### By Barun Basnet ######
-##### Friday, June 22 ######
+##### Friday, July 26 ######
 
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+from scipy.signal import max_len_seq
 
-A = np.array([1,1,1,1,1,1]) # No of LFSR = 6
+PN = max_len_seq(5)[0]  #Maximum Length Sequence(MLS) code generation
+PN = (PN * 2) - 1 # +1 and -1
 
-size = 63  #2^D - 1
-
+########################################################
+""" Manual Method (Without Library)
+A = np.array([1,1,1,1,1]) # No of LFSR = 5
+size = 31  #2^D - 1
 PN = [] # Storing generated Pseudo Noise sequence
-
-
 for i in range(size):
     print(A[-1])
     PN.append(A[-1])
-    A = [A[-1] ^ A[-2], A[0], A[1], A[2], A[3], A[4]] # ^ XOR
-    
-########################################################## 
-## Matching with voltage levels, i.e 1 = -1, 0 = 1       
-for i in range(size):
-    if PN[i]==1:
-        PN[i]=-1;
-    else:
-        PN[i]=1
-################################### 
-### Injected Signal #######
-### SSTDR/STDR ################
-        
-x = np.arange(0, 63) 
-carrier = np.sin(x)   
-inject = PN * carrier
-plt.plot(x, PN)
-plt.plot(x, carrier)
-plt.plot(x, inject)
+    A = [A[-1] ^ A[-2], A[0], A[1], A[2], A[3]] # ^ XOR """
+#########################################################################   
+######## Injected Signal for STDR/SSTDR ################      
+       
+x = np.arange(0, 1+0.01, 0.01) 
+Carrier = np.sin(2 * np.pi * x)
+
+Inject = []
+for i in range(len(PN)):
+  Inject = Inject + [PN[i]*Cr for Cr in Carrier]
+
+
+plt.plot(PN)
+plt.plot(Carrier)
+plt.plot(Inject)
 ####################################
 ####### Reflected Signal ###########
 
-noise = (np.random.normal(0, 1, 63)) * 0.5  # Approximation on white noise
-reflect = (inject + noise) * 0.8            #Attenuation of the signal
-plt.plot(x, reflect)
+Noise = (np.random.normal(0, 1, len(Inject))) #*0.5 # Approximation on white noise
+plt.plot(Noise)
+
+Reflect = (Inject + Noise) #* 0.8          #Attenuation of the signal
+plt.plot(Reflect)
 #######################################
 ###### Auto-correlation################
 
-result = np.correlate(inject, reflect, mode="same")
-plt.plot(x, result)
+result = np.correlate(Inject, Reflect, mode="same")
+plt.plot(result)
 
 #######################################
-######Normalized Auto-correlation######
+######Normalized Auto-correlation (-1 to +1) ######
 
 xx, yy = 0, 0
 
-for i in range(63):
-    xx = xx + inject[i]**2
-    yy = yy + reflect[i]**2
+for i in range(len(Inject)):
+    xx = xx + Inject[i]**2
+    yy = yy + Reflect[i]**2
     
 zz = math.sqrt(xx*yy)
-plt.plot(x, result/zz)
+plt.plot(result/zz)
+#############################################
+
+
+
+
+
+
+
